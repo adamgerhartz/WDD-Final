eed20ce3e5187627b154dbd848a2ea2963d0f136
Adam Gerhartz <adamgerhartz@Adams-Mac-mini.local>
2020-12-05 22:54:22 -0700

    Test

--- models/registrationModel.js
+++ models/registrationModel.js
@@ -7,7 +7,7 @@ function addUser(user, callback) {
 	console.log(`Adding user: ${user.username}`);
 
 	const statement = {
-		text: "INSERT INTO public.user (username, password, email_address, first_name, last_name, creation_date, last_update_date) VALUES ($1::text, $2::text, $3::text, :$4::text, $5::text, NOW(), NOW());",
+		text: 'INSERT INTO public.user (username, password, email_address, first_name, last_name, creation_date, last_update_date) VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, NOW(), NOW());',
 		values: [user.username, user.password, user.email, user.firstname, user.lastname]
 	}
 
